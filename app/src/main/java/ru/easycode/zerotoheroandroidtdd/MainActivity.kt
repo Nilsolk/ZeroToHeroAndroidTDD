cdc500b5dc9ab01eaaf38e4bc69c79b6e5689ed7
Nilsolk <nilsolk.bat@gmail.com>
2023-11-18 06:29:10 +0300

    Task008Test is done

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,39 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var visibility: Int = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.titleTextView)
+
+        if (savedInstanceState != null) textView.visibility =
+            savedInstanceState.getInt(VISIBILITY_KEY)
+
+
+
+        findViewById<Button>(R.id.hideButton).setOnClickListener {
+            textView.visibility = View.INVISIBLE
+            visibility = textView.visibility
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(VISIBILITY_KEY, visibility)
+    }
+
+    companion object {
+        private const val VISIBILITY_KEY = "KEY"
     }
 }
